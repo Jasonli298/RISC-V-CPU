b792c37082f38b110aad2fdaa22886c13039df11
Yuan Li <listeven199@gmail.com>
2023-02-05 16:27:05 -0800

    new file:   test/Mat_Vec_Mult.c

---
+++ test/Mat_Vec_Mult.c
@@ -0,0 +1,7 @@
+// This file is for the benchmark testing of matrix-vector-multiplication
+// meant to be easy-on-the-eye equivalent of RISC-V assembly version of the same program
+
+int main(char** argv, int argc){
+	
+	return 0;
+}
