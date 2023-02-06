6b42abb08232c4301b5d9910e9c7c9952cc5fb85
Yuan Li <listeven199@gmail.com>
2023-02-05 22:52:47 -0800

    modified:   test/Mat_Vec_Mult.c

--- test/Mat_Vec_Mult.c
+++ test/Mat_Vec_Mult.c
@@ -1,7 +1,39 @@
-// This file is for the benchmark testing of matrix-vector-multiplication
+// This program is for the benchmark testing of matrix-vector-multiplication
 // meant to be easy-on-the-eye equivalent of RISC-V assembly version of the same program
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(char** argv, int argc){
-	
+int N; // the number of columns in the matrix and the number of rows in the vector
+int R; // the number of rows in the matrix
+
+void MatVecMult(int matrix[R][N], const int vector[N], int res[N])
+{
+	int i, j; // i=rows, j=columns
+	for (i = 0; i < R; i++)
+	{
+		res[i] = 0;
+		for (j = 0; j < N; j++)
+		{
+			res[i] += matrix[i][j] * vector[j];
+		}
+	}
+}
+
+int main()
+{
+	R = 3;
+	N = 4;
+	int Matrix[3][4] = {{1, 2, 3, 4},
+						{-2, 6, 7, 0},
+						{4, 3, 2, 1}};
+
+	int Vector[4] = {1, 0, 2, 1};
+	int Res[4];
+
+	MatVecMult(Matrix, Vector, Res);
+	for (int i = 0; i < R; i++)
+	{
+		printf("%i\n", Res[i]);
+	}
 	return 0;
 }
