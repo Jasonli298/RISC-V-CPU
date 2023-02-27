92b87693a5c85f9c1dbf49f0a867279946d77c33
Yuan Li <listeven199@gmail.com>
2023-02-27 13:00:47 -0800

    modified:   test/tb_RISCV.v

--- test/tb_RISCV.v
+++ test/tb_RISCV.v
@@ -90,7 +90,10 @@ initial begin
 	$display("Generated Reseult");
 	for (i = M*N*4+N*N2*4; i <= M*N*4+N*N2*4+(M*N2-1)*4; i = i + 4*N2) begin
 		for (j = 0; j < N2; j = j + 1) begin
-			word = {UUT.D_Memory[i*N2+j], UUT.D_Memory[i*N2+j+1], UUT.D_Memory[i*N2+j+2], UUT.D_Memory[i*N2+j+3]};
+			word = {UUT.D_Memory[i*N2 + j],
+					UUT.D_Memory[i*N2 + j+1],
+					UUT.D_Memory[i*N2 + j+2],
+					UUT.D_Memory[i*N2 + j+3]};
 			$write("%d ", word);
 		end
 		$display();
@@ -99,10 +102,11 @@ initial begin
 	comparison = 1'b0;
 	for (i = 0; i < M; i = i + 1) begin
 		for (j = 0; j < N2; j = j + 1) begin
-			word = {UUT.D_Memory[i*4+M*N*4+N*N2*4+j],
-					UUT.D_Memory[i*4+M*N*4+N*N2*4+j+1],
-					UUT.D_Memory[i*4+M*N*4+N2*N*4+j+2],
-					UUT.D_Memory[4*i+M*N*4+N*N2*4+j+3]};
+			word = {UUT.D_Memory[i*4+M*N*4 + N*N2*4 + j],
+					UUT.D_Memory[i*4+M*N*4 + N*N2*4 + j+1],
+					UUT.D_Memory[i*4+M*N*4 + N*N2*4 + j+2],
+					UUT.D_Memory[i*4+M*N*4 + N*N2*4 + j+3]};
+			
 			if (res[N2*i+j] != word) begin
 				$display("Mismatch at indices [%1.1d,%1.1d]", i, j);
 				comparison = 1'b1;
