7f279b728fe59cb00d379def95cd32197956f96f
yibinpeter <60949214+yibinpeter@users.noreply.github.com>
2023-04-18 21:05:36 -0700

    Update mat_mul_new.s

--- test/mat_mul_new.s
+++ test/mat_mul_new.s
@@ -1,60 +1,18 @@
-# RISC-V assembly program for integer matrix-vector multiplication
 
-# Define the dimensions of the matrix and vector
-#define M 3
-#define N 4
-
-.data
-
-# Define the matrix as a contiguous array of signed integers
-matrix:
-  .word 1, 2, 3, 4
-  .word 5, 6, 7, 8
-  .word 9, 10, 11, 12
-
-# Define the vector as a contiguous array of signed integers
-vector:
-  .word 1, 2, 3, 4
-  
- result:
-  .word 0, 0, 0
-
-.text
-
-# Main function
-.globl main
 main:
   # Initialize variables
-  # li x4, M   # x4 = M
   addi x1, zero, M
-  # li x3, N   # x3 = N
   addi x2, zero, N
   addi x14, zero, 4 // x14 = 4
   mul x12, x2, x14 // x12 = 4 * N
   mul x3, x1, x12 // starting address of vector
-  # la x4, matrix   # x5 = address of matrix
   addi x4, x4, 0x0 // starting address of matrix
-  # lui x5, %hi(matrix)
-  # addi x5, x5, %lo(matrix)
-  # la x3, vector   # t3 = address of vector
-  # lui t3, %hi(vector)
-  # addi x5, x5, %lo(vector)
-  # la x5, result   # t4 = address of result
   add x5, x3, x12 // starting address of result
-  # lui t4, %hi(result)
-  # addi t4, t4, %lo(result)
-  # lw t3, 0(x3)  # x7 = first element of vector
-
-  # Outer loop: iterate over rows of matrix
-  # li x6, 0   # x6 = row index
   addi x6, x6, 0
+  
 outer_loop:
-  # Inner loop: iterate over columns of matrix and vector
-  # li t7, 0   # t7 = column index
   addi x7, zero, 0
-  # addi x6, x6, 1
 inner_loop:
-  # Load matrix element into t8
   lw x8, 0(x4)
   lw x10, 0(x3) // load vector element
 
